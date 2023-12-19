be6a0f0e000596aefa098b432a50b8896cedfe17
lucky-long1 <282484650@qq.com>
2023-12-19 02:01:03 -0500

    modify a.c

--- a.c
+++ a.c
@@ -2,5 +2,15 @@
 
 int main()
 {
-	printf("hello world !\n");
+	//printf("hello world !\n");
+	int a, j;
+	for (a=1;a>10;a++)
+	{
+		for (j=1;j>=a;j++)
+		{
+		printf("%d * %d = %d",j,a,j*a);
+		if (a == j)
+			printf("\n");
+		}
+	}
 }
